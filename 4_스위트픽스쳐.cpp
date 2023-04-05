b3a3a38f5601287f7d6fe70c1f4d784a85498959
Chansik Yun <chansigi@gmail.com>
2023-04-05 07:37:35 +0000

    .

--- 4_스위트픽스쳐.cpp
+++ 4_스위트픽스쳐.cpp
@@ -14,6 +14,38 @@ public:
 
 #include <gtest/gtest.h>
 
+class TerminalTest : public testing::Test {
+protected:
+    Terminal* term = nullptr;
+
+    void SetUp() override
+    {
+        term = new Terminal;
+        term->Connect();
+    }
+
+    void TearDown() override
+    {
+        term->Disconnect();
+        delete term;
+    }
+};
+
+TEST_F(TerminalTest, Login)
+{
+    term->Login("test_id", "test_password");
+    ASSERT_TRUE(term->IsLogin()) << "로그인 하였을 때";
+}
+
+TEST_F(TerminalTest, Logout)
+{
+    term->Login("test_id", "test_password");
+    term->Logout();
+
+    ASSERT_FALSE(term->IsLogin()) << "로그아웃 하였을 때";
+}
+
+#if 0
 TEST(TerminalTest, Login)
 {
     Terminal* term = new Terminal;
@@ -40,3 +72,4 @@ TEST(TerminalTest, Logout)
     term->Disconnect();
     delete term;
 }
+#endif
