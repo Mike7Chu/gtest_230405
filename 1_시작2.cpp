a789e61acc02cae372b488bd4caf44f5533575e3
Chansik Yun <chansigi@gmail.com>
2023-04-05 04:27:59 +0000

    .

---
+++ 1_시작2.cpp
@@ -0,0 +1,17 @@
+// 1_시작2.cpp
+#include <gtest/gtest.h>
+
+// Google Test 에서 테스트케이스를 작성하는 방법
+// : TEST(테스트 스위트 이름, 테스트 케이스 이름)
+
+// ImageTest.ResizeImage
+TEST(ImageTest, ResizeImage) { }
+
+// ImageTest.BlurImage
+TEST(ImageTest, BlurImage) { }
+
+TEST(SampleTest, First) { }
+
+// TestSuite
+//  - TestCase
+//  - TestCase
