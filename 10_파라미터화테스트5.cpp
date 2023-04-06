cb4b3777a8764efb8ebe4dc89fc8c3dacf80932d
Chansik Yun <chansigi@gmail.com>
2023-04-06 06:28:20 +0000

    .

--- 10_파라미터화테스트5.cpp
+++ 10_파라미터화테스트5.cpp
@@ -11,18 +11,27 @@ std::vector<std::string> cars = {
     "Sonata", "Avante", "Genesis"
 };
 
+int years[] = { 2010, 2020, 2030 };
+
 // class CarTest : public testing::TestWithParam<std::tuple<std::string, int>> { };
 
-using CarType = std::tuple<std::string, int>;
+using CarType = std::tuple<std::string, int, int>;
 class CarTest : public testing::TestWithParam<CarType> { };
+
+// testing::Combine
+// => 두 개 이상의 데이터를 조합해서, 사용할 수 있습니다.
 
 INSTANTIATE_TEST_SUITE_P(CarValues, CarTest,
     testing::Combine(
         testing::ValuesIn(cars),
-        testing::Values(RED, WHITE, BLACK)));
+        testing::Values(RED, WHITE, BLACK),
+        testing::ValuesIn(years)));
 
 TEST_P(CarTest, Sample)
 {
     auto& param = GetParam();
-    std::cout << std::get<0>(param) << ", " << std::get<1>(param) << std::endl;
+    std::cout << std::get<0>(param)
+              << ", " << std::get<1>(param)
+              << ", " << std::get<2>(param)
+              << std::endl;
 }
