adf28817a30742541c8062665db03e5dc77f63b4
Chansik Yun <chansigi@gmail.com>
2023-04-06 07:26:55 +0000

    .

---
+++ 12_테스트대역.cpp
@@ -0,0 +1,53 @@
+// 12_테스트대역.cpp
+
+// 테스트 대역(Test Double)
+#include <string>
+
+class FileSystem {
+public:
+    bool IsValidFilename(const std::string& filename)
+    {
+        // 현재의 파일 시스템에서 지원 가능한 파일 이름의 형태인지 확인합니다.
+        return true;
+    }
+};
+
+class Logger {
+public:
+    // 확장자를 제외한 파일의 이름이 5글자 이상이어야 한다.
+    // ex)
+    //  file.log => file  => invalid
+    // hello.log => hello => valid
+    bool IsValidLogFilename(const std::string& filename)
+    {
+        size_t index = filename.find_last_of(".");
+        std::string name = filename.substr(0, index);
+        if (name.size() < 5) {
+            return false;
+        }
+
+        // 현재의 파일시스템에서 가능한 이름인지 확인이 필요합니다.
+        FileSystem fs;
+        return fs.IsValidFilename(filename);
+    }
+};
+
+#include <gtest/gtest.h>
+
+TEST(LoggerTest, IsValidLogFilename_NameLonggerThan5Chars_ReturnsTrue)
+{
+    Logger logger;
+    std::string validFilename = "valid.log";
+
+    EXPECT_TRUE(logger.IsValidLogFilename(validFilename))
+        << "확장자를 제외한 파일명이 다섯글자 이상일 때";
+}
+
+TEST(LoggerTest, IsValidLogFilename_NameShorterThan5Chars_ReturnFalse)
+{
+    Logger logger;
+    std::string invalidFilename = "bad.log";
+
+    EXPECT_FALSE(logger.IsValidLogFilename(invalidFilename))
+        << "확장자를 제외한 파일명이 다섯글자 미만일 때";
+}
