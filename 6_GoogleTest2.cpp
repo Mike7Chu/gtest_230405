0c1a792637c105f68315b29f1a5fadc2188bd7da
Chansik Yun <chansigi@gmail.com>
2023-04-06 01:48:07 +0000

    .

--- 6_GoogleTest2.cpp
+++ 6_GoogleTest2.cpp
@@ -13,6 +13,12 @@
 // > 제외도 가능합니다.
 // $ ./a.out --gtest_filter=Image*.*:-*.goo
 
+// - 원하는 종류의 테스트를 선택적으로 수행할 수 있습니다.
+//  => 테스트케이스를 만들 때, 이름 규칙을 잘 결정하는 것이 좋습니다.
+//  * 테스트의 성격에 따라서 접두를 사용했습니다.
+//   P_OpenFile / N_OpenFile / X_OpenFile
+//  => 가독성 뿐 아니라 필터의 기능을 활용하는데 도움이 됩니다.
+
 // ImageTest.foo
 TEST(ImageTest, foo) { }
 
