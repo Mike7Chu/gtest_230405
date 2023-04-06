da7a9605cb0ae847ae2ed95869b7b289a11ca742
Chansik Yun <chansigi@gmail.com>
2023-04-06 06:33:06 +0000

    .

---
+++ 10_파라미터화테스트6.cpp
@@ -0,0 +1,19 @@
+// 10_파라미터화테스트6.cpp
+#include <gtest/gtest.h>
+
+class SampleTest : public testing::TestWithParam<int> { };
+
+// 0 - 99
+// 1) testing::Range(start, end)
+//  - [start, end)
+
+// 2) testing::Range(start, end, step)
+
+INSTANTIATE_TEST_SUITE_P(
+    SampleValues, SampleTest,
+    testing::Range(0, 100, 3));
+
+TEST_P(SampleTest, Sample)
+{
+    std::cout << GetParam() << std::endl;
+}
