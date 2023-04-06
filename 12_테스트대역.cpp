e202d1fd34eda664e06f90de05fa1c659bedd502
Chansik Yun <chansigi@gmail.com>
2023-04-06 07:31:46 +0000

    .

--- 12_테스트대역.cpp
+++ 12_테스트대역.cpp
@@ -1,6 +1,5 @@
 // 12_테스트대역.cpp
 
-// 테스트 대역(Test Double)
 #include <string>
 
 class FileSystem {
@@ -8,7 +7,7 @@ public:
     bool IsValidFilename(const std::string& filename)
     {
         // 현재의 파일 시스템에서 지원 가능한 파일 이름의 형태인지 확인합니다.
-        return true;
+        return false; // !!
     }
 };
 
@@ -20,17 +19,29 @@ public:
     // hello.log => hello => valid
     bool IsValidLogFilename(const std::string& filename)
     {
+        // --- 테스트 대상 코드 ---
         size_t index = filename.find_last_of(".");
         std::string name = filename.substr(0, index);
         if (name.size() < 5) {
             return false;
         }
+        // --- 테스트 대상 코드 ---
 
         // 현재의 파일시스템에서 가능한 이름인지 확인이 필요합니다.
         FileSystem fs;
         return fs.IsValidFilename(filename);
     }
 };
+//-------
+
+// 문제점
+//  Logger
+//    IsValidLogFilename ------X------> FileSystem.IsValidFilename
+//          |
+//          -----------------> TestDouble
+
+// 테스트 대역(Test Double)
+//  - 목적: 테스트 환경을 통제하기 위해서 사용합니다.
 
 #include <gtest/gtest.h>
 
