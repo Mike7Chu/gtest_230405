f5a2f9919a6dfa5224bf96418ad9dba087341354
Chansik Yun <chansigi@gmail.com>
2023-04-07 07:11:18 +0000

    .

--- 20_행위기반검증2.cpp
+++ 20_행위기반검증2.cpp
@@ -19,3 +19,29 @@ public:
     MOCK_METHOD(void, Third, (), (override));
     MOCK_METHOD(void, Forth, (), (override));
 };
+
+void Process(Dog* p)
+{
+    p->First();
+    p->Second();
+    p->Third();
+    p->Forth();
+}
+
+// 4. 함수 호출 순서
+//  - First -> Second -> Third -> Forth
+//  - InSquence
+using testing::InSequence;
+
+TEST(DogTest, Sample1)
+{
+    InSequence seq; // !!!
+    MockDog mock;
+
+    EXPECT_CALL(mock, First);
+    EXPECT_CALL(mock, Second);
+    EXPECT_CALL(mock, Forth);
+    EXPECT_CALL(mock, Third);
+
+    Process(&mock);
+}
