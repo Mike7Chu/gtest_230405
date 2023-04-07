c4de00da15466a5055c58c29e31af82b70ea53f3
Chansik Yun <chansigi@gmail.com>
2023-04-07 02:46:11 +0000

    .

--- 16_Mock.cpp
+++ 16_Mock.cpp
@@ -70,10 +70,17 @@ public:
 // 1. 모의 객체 - Mocking
 class MockTarget : public DLoggerTarget {
 public:
-    // void Write(Level level, const std::string& message) override
-    // - MOCK_METHOD{인자개수}(함수이름, 함수타입)
+        // - MOCK_METHOD{인자개수}(함수이름, 함수타입)
 
-    MOCK_METHOD2(Write, void(Level level, const std::string& message));
+    // 1.10 이전
+    // MOCK_METHOD2(Write, void(Level level, const std::string& message));
+
+    // 1.10 이후
+    // => MOCK_METHOD 하나로 통일되었습니다.
+    // => MOCK_METHOD(반환타입, 이름, 인자 정보, 한정자)
+
+    // void Write(Level level, const std::string& message) override
+    MOCK_METHOD(void, Write, (Level level, const std::string& message), (override));
 };
 
 TEST(DLoggerTest, Write)
@@ -116,3 +123,23 @@ TEST(DLoggerTest, Write)
     EXPECT_TRUE(t2.IsReceived(testLevel, testMessage));
 }
 #endif
+
+// 테스트 대역(Test Double) => xUnit Test Pattern
+// 1. Test Stub
+// => 특수한 상황을 시뮬레이션합니다.
+// => 협력 객체가 SUT가 테스트하는데 필요한 결과를 주도록 제어합니다.
+
+// 2. Fake Object
+// => 가벼운 테스트 대역을 통해, 아직 구현되지 않은 사용하기 어려운, 너무 느린 협력 객체를
+//    이용하는 SUT를 검증합니다.
+
+// 3. Test Spy
+// => SUT에 관찰할 수 있는 부수효과가 존재하지 않는 경우,
+//    협력 객체를 통해 목격한 일을 기록해두었다가, 검증에 사용합니다.
+
+// 4. Mock Object
+// => SUT에 관찰할 수 있는 부수효과가 존재하지 않는 경우,
+//    행위 기반 검증을 수행합니다.
+
+// Google Mock => 테스트 대역 프레임워크
+//  => Google Mock을 이용해서, Stub, Fake도 만들 수 있습니다.
