bf997acabaafa7fe734dda9570bef945cd6ad6e7
Chansik Yun <chansigi@gmail.com>
2023-04-07 05:48:00 +0000

    .

---
+++ 19_Delegating.cpp
@@ -0,0 +1,57 @@
+// 19_Delegating.cpp
+#include <iostream>
+
+class Calc {
+public:
+    virtual ~Calc() { }
+
+    virtual int Add(int a, int b)
+    {
+        std::cout << "Calc::Add" << std::endl;
+        return a + b;
+    }
+
+    virtual int Sub(int a, int b)
+    {
+        return a - b;
+    }
+};
+
+void Process(Calc* p)
+{
+    if (p->Add(10, 20) == 30) {
+        int result = p->Sub(100, 20);
+        std::cout << "Success: " << result << std::endl;
+    } else {
+        std::cout << "Fail" << std::endl;
+    }
+}
+
+//----
+#include <gmock/gmock.h>
+
+// 1. MOCK_METHOD한 함수는 결과가 int()입니다. => 0
+// 2. MOCK_METHOD한 함수에 대한 결과를 제어할 수 있습니다.
+//   : ON_CALL(mock, Add(10, 20)).WillByDefault(...)
+
+class MockCalc : public Calc {
+public:
+    MOCK_METHOD(int, Add, (int a, int b), (override));
+    MOCK_METHOD(int, Sub, (int a, int b), (override));
+};
+
+using testing::Return;
+
+TEST(ProcessTest, Sample)
+{
+    MockCalc mock;
+    ON_CALL(mock, Add(10, 20)).WillByDefault(Return(30));
+    ON_CALL(mock, Sub(100, 20)).WillByDefault(Return(80));
+
+    EXPECT_CALL(mock, Add(10, 20));
+    EXPECT_CALL(mock, Sub(100, 20));
+
+    // std::cout << mock.Add(10, 20) << std::endl;
+
+    Process(&mock);
+}
