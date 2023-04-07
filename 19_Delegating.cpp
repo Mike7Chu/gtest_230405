11c41d3091606bb9b9933a174057a6edbb105fd6
Chansik Yun <chansigi@gmail.com>
2023-04-07 05:50:06 +0000

    .

--- 19_Delegating.cpp
+++ 19_Delegating.cpp
@@ -33,6 +33,7 @@ void Process(Calc* p)
 // 1. MOCK_METHOD한 함수는 결과가 int()입니다. => 0
 // 2. MOCK_METHOD한 함수에 대한 결과를 제어할 수 있습니다.
 //   : ON_CALL(mock, Add(10, 20)).WillByDefault(...)
+//  - 함수/함수객체/람다표현식
 
 class MockCalc : public Calc {
 public:
@@ -42,11 +43,16 @@ public:
 
 using testing::Return;
 
+int AddImpl(int a, int b) { return a + b; }
+
 TEST(ProcessTest, Sample)
 {
     MockCalc mock;
-    ON_CALL(mock, Add(10, 20)).WillByDefault(Return(30));
-    ON_CALL(mock, Sub(100, 20)).WillByDefault(Return(80));
+    // ON_CALL(mock, Add(10, 20)).WillByDefault(Return(30));
+    // ON_CALL(mock, Sub(100, 20)).WillByDefault(Return(80));
+    // ON_CALL(mock, Add).WillByDefault(AddImpl);
+    ON_CALL(mock, Add).WillByDefault([](int a, int b) { return a + b; });
+    ON_CALL(mock, Sub).WillByDefault([](int a, int b) { return a - b; });
 
     EXPECT_CALL(mock, Add(10, 20));
     EXPECT_CALL(mock, Sub(100, 20));
